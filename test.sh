d0decafa8e540a980faa207bfa51e639a6f71513
gauravksahni <gauravksahni@gmail.com>
2022-08-18 20:58:36 +0530

    updated the test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Calculate the sum of two integers with parametrized values
+# Calculate the sum of two integers with parametrized values in jenkins 
 # in a shell script
  
 sum=$(( $num1 + $num2 ))
